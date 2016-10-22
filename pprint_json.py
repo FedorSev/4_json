d094cdac3f0b4106af0eabaad5a31edd9f3a6856
fedor <tdgeccrew@gmail.com>
2016-10-22 17:58:38 +0300

    pretty_print

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,26 @@
 import json
+import os
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath) or os.path.isdir(filepath):
+        return None
+
+    with open(filepath,'r') as file_handler:
+        return json.load(file_handler)
 
 
 def pretty_print_json(data):
-    pass
-
+    print (json.dumps(data, sort_keys = True, indent = 4, ensure_ascii = False))
 
 if __name__ == '__main__':
-    pass
+    while True:
+        filepath = input('Введите путь к файлу: ')
+        data = load_data(filepath)
+
+        if data != None:
+            pretty_print_json(data)
+            break
+
+        else:
+            print('Некорректный адрес файла, повторите ввод')
