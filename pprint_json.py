777c7c758b1aa76100db0d35fbc7bf4c0450c0a2
fedor <tdgeccrew@gmail.com>
2016-10-30 22:42:24 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -6,19 +6,19 @@ def load_data(filepath):
     if not os.path.exists(filepath) or os.path.isdir(filepath):
         return None
 
-    with open(filepath,'r') as file_handler:
+    with open(filepath, 'r') as file_handler:
         return json.load(file_handler)
 
 
 def pretty_print_json(data):
-    print (json.dumps(data, sort_keys = True, indent = 4, ensure_ascii = False))
+    print (json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False))
 
 if __name__ == '__main__':
     while True:
         filepath = input('Введите путь к файлу: ')
         data = load_data(filepath)
 
-        if data != None:
+        if data is not None:
             pretty_print_json(data)
             break
 
